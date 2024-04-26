65eb21ec13e452be176946328f2d9397143871c0
Alex Lemna <git@alexanderlemna.net>
2024-04-26 11:40:25 -0500

    Add docstrings
    Added docstrings to the enumerated OSs, and to the
    properties of the `OperatingSystem` dataclasses.
    
    Changes to be committed:
    	modified:   src/lmn/cx/y22/operating_systems/_operating_systems.py

--- src/lmn/cx/y22/operating_systems/_operating_systems.py
+++ src/lmn/cx/y22/operating_systems/_operating_systems.py
@@ -32,8 +32,17 @@ class OperatingSystem:
     system should follow the XDG Base Directory Specification."""
 
     os_name: str
+    """The name of the operating system."""
+
     unix_like: bool = False
+    """Whether or not the operating system is 'Unix-like'. This comes down
+    to my arbitrary decision, which generally boils down to, 'Should an
+    application running on this OS assume it can access a filesystem with
+    a FHS-like layout?'"""
+
     xdg: bool | None = None
+    """Whether or not an application running on this operating system
+    should follow the XDG Base Directory Specification."""
 
     def __post_init__(self):
         if self.xdg is None:
@@ -41,15 +50,74 @@ class OperatingSystem:
 
 
 class OS(OperatingSystem, Enum):
+
     ANDROID: Final = "Android"
+    """The Android operating system is a mobile operating system with
+    a free, open-source core, running the Linux kernel. Almost all 
+    consumer devices running Android run Google's proprietary version
+    of Android, but other Android forks include Amazon's Fire OS and
+    the community-based LineageOS. Applications on it should not 
+    follow the XDG.
+
+    See [the Android Open Source Project website](https://source.android.com/)."""
+
     FREEBSD: Final = "FreeBSD", True  # ("FreeBSD", unix_like=True)
+    """FreeBSD is a free, open-source, Unix-like operating system
+    known for its use of the ZFS file system and high-performance
+    networking stack. It excels when used as a web server, but
+    especially as a file server. It was the operating system
+    behind the PlayStation 3 and PlayStation 4. 
+    
+    See [the FreeBSD website](https://www.freebsd.org/)."""
+
     iOS: Final = "iOS"
+    """Apple's iOS is a mostly closed-source mobile operating
+    system, based on its macOS desktop operating system. Apple's
+    iPhones run iOS, and it's also the basis for Apple's tvOS, iPadOS,
+    and watchOS. Applications on it should not follow the XDG.
+    
+    See [the iOS website](https://www.apple.com/ios/)."""
+
     LINUX: Final = "Linux", True  # ("Linux", unix_like=True)
+    """Linux is a family of open-source, Unix-like operating systems
+    based around the Linux kernel. Popular distributions include
+    Ubuntu, Debian, and Fedora.
+    
+    See [the Linux kernel website](https://kernel.org/)."""
+
     MAC: Final = "macOS", True, False  # ("macOS", unix_like=True, xdg=False)
+    """Apple macOS is a mostly closed-source operating system that
+    runs on Apple's desktops and laptops. The core of the operating 
+    system remains open-sourced as the 'Darwin' operating system.
+    Applications on it should not follow the XDG.
+    
+    See [the macOS website](https://apple.com/macos)."""
+
     NETBSD: Final = "NetBSD", True  # ("NetBSD", unix_like=True)
+    """NetBSD is a free, open-source, portability-focused Unix-like
+    operating system that supports a large number of hardware
+    platforms. 
+    
+    See [the NetBSD website](https://netbsd.org/)."""
+
     OPENBSD: Final = "OpenBSD", True  # ("OpenBSD", unix_like=True)
+    """OpenBSD is a free, open-source, security-focused Unix-like
+    operating system with strengths in cryptography, simplicity, and
+    useful documentation. It excels when used as a firewall or router,
+    or as a server for core network services like DNS and NTP.
+    
+    See [the OpenBSD website](https://www.openbsd.org/)."""
+
     UNKNOWN: Final = "UNKNOWN"
+    """Any operating system not specified here."""
+
     WINDOWS: Final = "Windows"
+    """Microsoft Windows is a closed-source operating system often
+    used on desktops and laptops, and on enterprise servers. It's the
+    most-used operating system in the world for laptops and desktops.
+    Applications on it should not follow the XDG.
+    
+    See [the Windows website](https://windows.com/)."""
 
 
 def determine_os() -> OS:
