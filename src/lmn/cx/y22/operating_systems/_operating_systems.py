2ca57696c26047568ddb5e8065ab569a8a9eb6b3
Alex Lemna <git@alexanderlemna.net>
2024-04-26 11:20:15 -0500

    Fix failing tests (requires API break)
    The `xdg` value for the `OperatingSystem` instances was supposed
    to have a default value that depended on that instance's `unix_like`
    value, but that wasn't working.
    
    After playing around with metaclasses and decorators and things,
    I decided the easiest way forward was just to handle it as a
    `__post_init__` method and call it a day. No need to think about
    `__call__`, etc., to modify arguments being passed to
    `OperatingSystem.__init__`.
    
    As a side effect of all of this, `OperatingSystem` no longer takes
    other `OperatingSystem` objects as an initialization argument. This
    is the API break.
    
    Changes to be committed:
    	modified:   src/lmn/cx/y22/operating_systems/_operating_systems.py

--- src/lmn/cx/y22/operating_systems/_operating_systems.py
+++ src/lmn/cx/y22/operating_systems/_operating_systems.py
@@ -22,7 +22,7 @@ except importlib.metadata.PackageNotFoundError:
     __version__ = "UNKNOWN"
 
 
-@dataclass(frozen=True, init=False)
+@dataclass(frozen=True)
 class OperatingSystem:
     """A dataclass representing an operating system. Its properties:
     - os_name (str): The name of the operating system.
@@ -32,54 +32,24 @@ class OperatingSystem:
     system should follow the XDG Base Directory Specification."""
 
     os_name: str
-    unix_like: bool
-    xdg: bool
+    unix_like: bool = False
+    xdg: bool | None = None
 
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        if (
-            "xdg" not in kwds.keys()
-            and "unix_like" in kwds.keys()
-            and kwds["unix_like"] is True
-        ):
-            kwds["xdg"] = True
-
-        return self.__init__(*args, **kwds)
-
-    def __init__(
-        self,
-        name_or_os: "str | OperatingSystem",
-        unix_like: bool = False,
-        xdg: bool = False,
-    ) -> None:
-        # if we're being initialized with an argument that is
-        # already an OperatingSystem, then we should just make
-        # ourselves identical to it. After all, int(int(x))
-        # should be the same as int(x).
-        if isinstance(name_or_os, OperatingSystem):
-            _os = name_or_os
-            object.__setattr__(self, "os_name", _os.os_name)
-            object.__setattr__(self, "unix_like", _os.unix_like)
-            object.__setattr__(self, "xdg", _os.xdg)
-
-        # Otherwise, we can assume the first argument we're
-        # being initialized with is a string.
-        else:
-            os_name = name_or_os
-            object.__setattr__(self, "os_name", os_name)
-            object.__setattr__(self, "unix_like", unix_like)
-            object.__setattr__(self, "xdg", xdg)
+    def __post_init__(self):
+        if self.xdg is None:
+            object.__setattr__(self, "xdg", True if self.unix_like else False)
 
 
 class OS(OperatingSystem, Enum):
-    ANDROID: Final = OperatingSystem("Android")
-    FREEBSD: Final = OperatingSystem("FreeBSD", unix_like=True)
-    iOS: Final = OperatingSystem("iOS")
-    LINUX: Final = OperatingSystem("Linux", unix_like=True)
-    MAC: Final = OperatingSystem("macOS", unix_like=True, xdg=False)
-    NETBSD: Final = OperatingSystem("NetBSD", unix_like=True)
-    OPENBSD: Final = OperatingSystem("OpenBSD", unix_like=True)
-    UNKNOWN: Final = OperatingSystem("UNKNOWN")
-    WINDOWS: Final = OperatingSystem("Windows")
+    ANDROID: Final = "Android"
+    FREEBSD: Final = "FreeBSD", True  # ("FreeBSD", unix_like=True)
+    iOS: Final = "iOS"
+    LINUX: Final = "Linux", True  # ("Linux", unix_like=True)
+    MAC: Final = "macOS", True, False  # ("macOS", unix_like=True, xdg=False)
+    NETBSD: Final = "NetBSD", True  # ("NetBSD", unix_like=True)
+    OPENBSD: Final = "OpenBSD", True  # ("OpenBSD", unix_like=True)
+    UNKNOWN: Final = "UNKNOWN"
+    WINDOWS: Final = "Windows"
 
 
 def determine_os() -> OS:
